7704d0a27748bf0bacf1f37c6730e9b9fac235ea
Kazybek777 <151760232+Kazybek777@users.noreply.github.com>
2026-02-13 00:35:17 +0600

    add service/repositories

---
+++ backend/app/schemas/cart.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class CartItemBase(BaseModel):
+    product_id: int = Field(..., description="Product ID")
+    quantity: int = Field(..., gt= 0, description="Quantity (must be greater than 0)")
+
+
+class CartItemCreate(CartItemBase):
+    pass
+
+class CartItemUpdate(CartItemBase):
+    product_id: int = Field(..., description="Product ID")
+    quantity: int = Field(..., gt=0,
+                          description="Quantity (must be greater than 0)")
+
+class CartItem(BaseModel):
+    product_id: int
+    name: str = Field(..., description="Product name")
+    price: float = Field(..., description="Product price")
+    quantity: int = Field(..., description="Quantity in Cart")
+    subtotal: float = Field(...,
+                            description="Total price for this item (price + quantity)")
+    image_url: Optional[str] = Field(None, description="Product image url")
+
+
+class CartResponse(BaseModel):
+    items: list[CartItemBase] = Field(..., description="List of items in Cart")
+    total: int = Field(..., description="Total number of items in Cart")
+    items_count: int = Field(..., description="Number of items in Cart")
